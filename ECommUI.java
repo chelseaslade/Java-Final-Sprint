0200c2a4b0c16cdc7bf2e264bee606e80b94806f
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-11-27 12:50:02 -0330

    Comments for layout

--- ECommUI.java
+++ ECommUI.java
@@ -1,5 +1,22 @@
 public class ECommUI {
     public static void main(String[] args) {
-        
+        //Construct test objects (as needed)
+
+        //Loop for initial menu needed ("Exit" option closes entire program)
+
+        //Login/Register Menu Options
+        //1. Register
+        //2. Login
+        //3. Exit
+
+        //Loop for main menu needed ("Exit" option to take back to login)
+
+        //Main Menu Options (Depends on user type logged in)
+        //Admin Menu
+
+        //Buyer Menu
+
+        //Seller Menu
+
     }
 }
