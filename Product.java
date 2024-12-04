c68150b3d90156b2f9fc358dc31fb397e3f7f968
Coastaal <162140210+Coastaal@users.noreply.github.com>
2024-12-03 20:38:13 -0330

    product.java code added

--- Product.java
+++ Product.java
@@ -1,7 +1,48 @@
 public class Product {
-    //Attributes
+    private int id;
+    private String name;
+    private BigDecimal price; //bigdecimal for precise amt/$
+    private int quantity;
+    private int sellerId;
 
-    //Constructors
+    //constructors
+    public Product(String name, BigDecimal price, int quantity, int sellerId){
+        this.name = name;
+        this.price = price; 
+        this.quantity = quantity; 
+        this.sellerId = sellerId;
+    }
 
-    //Methods
+    //getters and setters
+    public int getId(){
+        return id; 
+    }
+    public void setId(int id){
+        this.id = id; 
+
+    }
+    public String getName(){
+        return name; 
+    }
+    public void setName(String name){
+        this.name = name
+    }
+    public BigDecimal getPrice(){
+        return price;
+    }
+    public void setPrice(BigDecimal price){
+        this.price = getPrice;
+    }
+    public int getQuantity(){
+        return quantity; 
+    }
+    public void setQuantity (int quantity){
+        this.quantity = quantity;
+    }
+    public in getSellerId(){
+        return sellerId;
+    }
+    public void setSellerId(int sellerId){
+        this.sellerId = sellerId;
+    }
 }
