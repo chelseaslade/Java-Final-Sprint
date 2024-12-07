b9e5f99f8841d53ebd4b59b56fcb84273a382f0b
Coastaal <162140210+Coastaal@users.noreply.github.com>
2024-12-07 16:18:02 -0330

    Update: Sellers getter/setter  & fetch - review?

--- Seller.java
+++ Seller.java
@@ -1,21 +1,54 @@
-
 public class Seller extends User {
 
     //Constructors
-    public Seller(String username, String password, String email)
-    {
+
+    public Seller(String username, String password, String email){
         super(username, password, email, "seller");
     }
 
-//     public void viewOwnedProducts(){
-//         List<Product> products = ProductDAO.getProductsBySeller(this.getId());
-//         if (products.isEmpty()) {
-//             System.out.println("Error: 0 listed any products.");
-//         } else {
-//         for (Product product : products) {
-//             System.out.println(product);          
-//         }
-//     }
-// }
+    //getter & setters 
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
 
+    @Override
+    public void setUsername(String username) {
+        super.setUsername(username);
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public void setPassword(String password) {
+        super.setPassword(password);
+    }
+
+    @Override
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    @Override
+    public void setEmail(String email) {
+        super.setEmail(email);
+    }
+
+    @Override
+    public String getRole() {
+        return super.getRole();
+    }
+
+    @Override
+    public void setRole(String role) {
+        super.setRole(role);
+    }
+
+    // fetch  products from the ProductDAO remove if not needed!
+    public List<Product> getProductsForSale(ProductDAO productDAO) {
+        return productDAO.getProductsBySellerId(this.getUsername());
+    }
 }
