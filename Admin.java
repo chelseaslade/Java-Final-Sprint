8280f22e51aaa4d92f19256918f7a95bb4b0abbc
Coastaal <162140210+Coastaal@users.noreply.github.com>
2024-12-07 16:30:16 -0330

    UPDATE: Admin getter/setter

--- Admin.java
+++ Admin.java
@@ -5,38 +5,61 @@ public class Admin extends User {
         super(username, password, email, "admin");
     }
 
-    //Methods
-    // public void viewAllUsers(){
-    //     List<User> users = UserDAO.getAllUsers();
-    //     if (users.isEmpty()){
-    //         System.out.println("Error: Users not found");
-    //     } else{
-    //         for (User user: users){
-    //             System.out.println(user:) // will display user details
-    //         }
-    //     }
-    // }
-    // public void deleteUser(int userId){
-    //     if (UserDAO.deleteUser(userId)){
-    //         System.out.println("User deleted"); 
-    //     } else{
-    //         System.out.println("Error: failed to delete (user)");
-    //     }
-    // }
-    // public void viewAllProducts(){
-    //     List<Product> products = ProductDAO.getAllProducts();
-    //     if (products.isEmpty()) {
-    //         System.out.println("Error: Product not listed.");
-    //     } else {
-    //     for (Product product : products) {
-    //         System.out.println(product);  // displays product detail. 
-    //     }
-    // }
-    // }
-    // public void deleteProduct(int productId){
-    //     if (ProductDAO.deleteProduct(productId)) {
-    //         System.out.println("Product deleted.");
-    //     } else {
-    //         System.out.println("Error: failed to delete product.");
-    // }
-}
+    //getters & setters
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public void setUsername(String username) {
+        super.setUsername(username);
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public void setPassword(String password) {
+        super.setPassword(password);
+    }
+
+    @Override
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    @Override
+    public void setEmail(String email) {
+        super.setEmail(email);
+    }
+
+    @Override
+    public String getRole() {
+        return super.getRole();
+    }
+
+    @Override
+    public void setRole(String role) {
+        super.setRole(role);
+    }
+    
+    
+    /* ADMIN METHOD - REVIEWED ECommUI SAW ACTION THERE
+        // delete user 
+    public void deleteUser(User user, UserDAO userDAO){
+       userDAO.deleteUser(user);
+    }
+        //return user
+    public void viewAllUsers(UserDAO userDAO){
+        userDAO.getAllUsers().forEach(user -> System.out.println(getUsername()+ "-" + user.getEmail()))
+    }
+        // view products
+    public void viewAllProducts(ProductDAO productDAO){
+        productDAO.getAllProducts().forEach(product->System.out.println(product.getName()+ "-" +product.getPrice()))
+    } */
+
+
+ 
