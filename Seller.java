6db02d4cc39e68823622951bfc510560029f6b7c
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-12-10 11:54:16 -0330

    Removed unused code

--- Seller.java
+++ Seller.java
@@ -7,15 +7,4 @@ public class Seller extends User {
         super(username, password, email, "seller");
     }
 
-//     public void viewOwnedProducts(){
-//         List<Product> products = ProductDAO.getProductsBySeller(this.getId());
-//         if (products.isEmpty()) {
-//             System.out.println("Error: 0 listed any products.");
-//         } else {
-//         for (Product product : products) {
-//             System.out.println(product);          
-//         }
-//     }
-// }
-
 }
